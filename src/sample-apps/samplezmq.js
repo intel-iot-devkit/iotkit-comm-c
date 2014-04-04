9c6eb221e863b40c60cb7c11d04ddd2c93a5eaa4
dtran <dzung.d.tran@intel.com>
2014-04-04 07:22:48 -0700

    removed commented out functions

--- src/sample-apps/samplezmq.js
+++ src/sample-apps/samplezmq.js
@@ -20,7 +20,6 @@ function onDiscovery(service){
 	console.log("found " + service.type.name + " service at " + service.addresses[service.addresses.length-1] + ":" + service.port);
 
     pubsubclient = new PubSubComm(service.addresses[service.addresses.length-1], service.port, 'sub');
-  // pubsubclient.subscribeFrom(service.addresses[service.addresses.length-1], service.port);
 
 	pubsubclient.subscribe('/Intel/temperature', function (topic, message) {
     console.log('msg ' + message);
