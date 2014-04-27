33b89d13cfe9ac51f03bd106b1af1a18b5c2f930
SakthivelX Kothur Gullachetty <sakthivelx.kothur.gullachetty@intel.com>
2014-04-27 19:30:54 +0000

    Updation to MQTT Plugin as per new API

---
+++ src-c/edison-lib/plugins/libmqttpubsub/edison-mqtt_async-client.h
@@ -0,0 +1,52 @@
+/*
+ * edison-mqtt.h
+ *
+ *  Created on: 2014-04-16
+ *      Author: pchenthx
+ */
+
+#ifndef EDISON_MQTT_ASYNC_H_
+#define EDISON_MQTT_ASYNC_H_
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+#include <MQTTAsync.h>
+
+#define CLIENTID    "ExampleClientPub"
+#define QOS         1
+#define TIMEOUT     10000L
+volatile MQTTAsync_token deliveredtoken;
+
+
+#ifndef DEBUG
+    #define DEBUG 1
+#endif
+
+volatile int toStop = 0;
+ volatile int finished = 0;
+ volatile int connected = 0;
+ volatile int quietMode = 0;
+ volatile int sent = 0;
+ volatile int delivery = 0;
+
+
+// TODO: use common header file for Context
+typedef struct _Context {
+    char *name;
+    char *value;
+} Context;
+
+
+int send(char *message, Context context);
+int subscribe(char *topic);
+int unsubscribe(char *topic);
+int setReceivedMessageHandler(void (*) (char *topic, Context context));
+//values for type --> open, ssl
+//int createClient(char *host, int port, char *type, void *sslargs);
+//int createService();
+int done();
+
+
+#endif /* EDISON_MQTT_ASYNC_H_ */
