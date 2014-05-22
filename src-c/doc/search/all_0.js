128615d4acad3f4c7ceefa3bfe4e80b6f4a0271e
pchenthx <pradeepx.chenthati@intel.com>
2014-05-22 15:39:54 -0700

    Removed unused code

--- src-c/doc/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['edison_2dmqtt_5fasync_2dclient_2ec',['edison-mqtt_async-client.c',['../edison-mqtt__async-client_8c.html',1,'']]],
-  ['edison_2dmqtt_5fasync_2dservice_2ec',['edison-mqtt_async-service.c',['../edison-mqtt__async-service_8c.html',1,'']]]
-];
